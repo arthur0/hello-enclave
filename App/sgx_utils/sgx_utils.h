6139fdcda072a1c45a5c8ef80ef6e1144e88eab7
Arthur Miranda <arthur.souzamiranda@gmail.com>
2018-07-12 01:01:59 -0300

    Improve outputs

--- App/sgx_utils/sgx_utils.h
+++ App/sgx_utils/sgx_utils.h
@@ -5,8 +5,8 @@
 
 void print_error_message(sgx_status_t ret);
 
-int initialize_enclave(sgx_enclave_id_t* eid, const std::string& launch_token_path, const std::string& enclave_name);
+int initialize_enclave(sgx_enclave_id_t *eid, const std::string &launch_token_path, const std::string &enclave_name);
 
-bool is_ecall_successful(sgx_status_t sgx_status, const std::string& err_msg, sgx_status_t ecall_return_value = SGX_SUCCESS);
+bool is_status_successful(sgx_status_t sgx_status, const std::string &err_msg, sgx_status_t ecall_return_value = SGX_SUCCESS);
 
 #endif // SGX_UTILS_H_
